4bc0a8910e33518b4586728d15730e34c984a5a8
EUNSUN <sunej10@naver.com>
2023-04-12 15:00:12 +0900

    초기환경세팅

---
+++ src/main/java/com/memo/dao/PostMapper.java
@@ -0,0 +1,14 @@
+package com.memo.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostMapper {
+
+	// input : 전체 select라 없음
+	// output : List다 불러와~~
+	public List<Map<String, Object>> selectPostList();
+}
